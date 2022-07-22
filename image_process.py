495b7ead8f22164d4c929d4e41f65ded67543e39
felixloftus <floftus@hotmail.co.uk>
2022-07-22 16:04:00 +0100

    test commit

--- image_process.py
+++ image_process.py
@@ -50,9 +50,9 @@ for i in range(15):
     solarised= PIL_processes.solarize(img3, 130)
     # # img4 = PIL_processes.PIL_invert(img4)
     composited1 = Image.composite(composited1, solarised, img3)
-
+    composited1 = PIL_processes.PIL_contrast(composited1, 1.25) 
     composited1.show()
-    composited1 = PIL_processes.PIL_contrast(composited1, 1.25)
+    
 
 
     width, height = composited1.size
